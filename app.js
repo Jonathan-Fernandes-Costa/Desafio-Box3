5730f0925f3f44d587de02d76a1cd23453c8743a
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-08-08 22:26:56 -0300

    :sparkles: feat: Atualização de Contatos por ID

--- app.js
+++ app.js
@@ -56,23 +56,23 @@ async function getContatobyId() {
         tabelaBody.innerHTML = "";
 
         // Adicionar cada item da API como uma linha na tabela
-            var newRow = tabelaBody.insertRow();
-            var cell1 = newRow.insertCell(0);
-            var cell2 = newRow.insertCell(1);
-            var cell3 = newRow.insertCell(2);
-            var cell4 = newRow.insertCell(3);
-            var cell5 = newRow.insertCell(4);
-            var cell6 = newRow.insertCell(5);
+        var newRow = tabelaBody.insertRow();
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+        var cell4 = newRow.insertCell(3);
+        var cell5 = newRow.insertCell(4);
+        var cell6 = newRow.insertCell(5);
 
-            cell1.textContent = data.id;
-            cell2.textContent = data.nome;
-            cell3.textContent = data.telefone;
-            cell4.textContent = data.email;
-            cell5.textContent = data.ativo;
-            cell6.textContent = data.dataNascimento;
-            // Adicione mais células conforme necessário
-        };
-    }
+        cell1.textContent = data.id;
+        cell2.textContent = data.nome;
+        cell3.textContent = data.telefone;
+        cell4.textContent = data.email;
+        cell5.textContent = data.ativo;
+        cell6.textContent = data.dataNascimento;
+        // Adicione mais células conforme necessário
+    };
+}
 
 async function postContatos() {
     let name = document.getElementById("name").value;
@@ -96,8 +96,38 @@ async function postContatos() {
         },
         body: JSON.stringify(infos)
     }
-    console.log(request.body)
     fetch(urlApi, request)
+        .then(res => res.json())
+        .then(resData => {
+            console.log("Resposta da API:", resData);
+        })
+        .catch(error => {
+            console.error("Erro na requisição:", error);
+        });
+}
+async function putContatobyId() {
+    let name = document.getElementById("namePUT").value;
+    let fone = document.getElementById("fonePUT").value;
+    let email = document.getElementById("emailPUT").value;
+    let status = document.getElementById("statusPUT").checked;
+    let data = document.getElementById("dataPUT").value;
+
+    let infos = {
+        nome: name,
+        telefone: fone,
+        email: email,
+        ativo: status,
+        dataNascimento: data
+    }
+    let request = {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(infos)
+    }
+    let id = document.getElementById("idPUT").value;
+    fetch(urlApi + "/" + id, request)
         .then(res => res.json())
         .then(resData => {
             console.log("Resposta da API:", resData);
